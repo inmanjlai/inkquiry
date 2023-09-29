bb2ca3d0e4318beddcef6b3517d280298ab6fe26
OmarMohamed543 <omohamed5297@gmail.com>
2023-09-29 03:29:09 -0400

    omar

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'pug')
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.render('index', { messageToDisplay: "HEY WELCOME TO PUG" });
 });
 
-app.get('/card', (req, res) => {
-    res.render('card', { card: {name: "Mickey Mouse", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat." } })
+async function getCard(cardName) {
+    const req = await fetch(`https://api.lorcana-api.com/strict/${cardName}`)
+    const res = await req.json()
+    return res;
+}
+
+app.post('/card', async(req, res) => {
+    console.log(req.body)
+    const card = await getCard(req.body.cardName)
+    console.log(card)
+    res.send("Looking For Card")
 })
 
 app.listen("8080", () => {
